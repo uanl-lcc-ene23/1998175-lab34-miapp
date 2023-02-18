6f5c0e2eeb5bfca287432bb2971f603d4dfd3a64
ZInePi29 <84595745+ZInePi29@users.noreply.github.com>
2023-02-17 20:16:42 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,8 @@
+var campoTexto = document.getElementById("nombreTXT");
+console.log(campoTexto);
+function imprimir(){
+        console.log(campoTexto.value)
+}
+function limpiar(){
+    campoTexto.value=""
+}
