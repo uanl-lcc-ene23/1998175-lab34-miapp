0ba3fb909247c72617fa1824cb3e5d8ca351dad3
ZInePi29 <84595745+ZInePi29@users.noreply.github.com>
2023-04-23 18:36:30 -0600

    practica7

--- miApp/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.css']
 })
-export class ListaAlumnosComponent {
+export class ListaAlumnosComponent implements OnInit{
+  
+  constructor(private actionSheetCtrl: ActionSheetController){ }
 
+  ngOnInit(): void {
+  }
+  nombre = "Francisco";
+  alumnos : any = ["Beto", "carlos", "Daniela", "eby", "alejandra", "daniel"];
+  
+  mostrarMensaje(){
+    alert("hola. mundo")
+  }
+  
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Eliminar alumno',
+      subHeader: 'Elimina al alumno de la lista',
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    }); 
+    
+    await actionSheet.present();
+  }
 }
